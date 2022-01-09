557e7d972e4ae4c6b0e5ccb348c92993b3af90e7
valeryan15 <valeryan15@gmail.com>
2022-01-09 22:20:05 +0700

    Подключил базу данных для чата

--- server/src/index.js
+++ server/src/index.js
@@ -4,35 +4,24 @@ import cors from 'cors'
 import http from 'http'
 import routes from './routes'
 import swaggerSpec from './OpenAPISpecification'
+import bodyParser from "body-parser"
+import swaggerUi from 'swagger-ui-express'
 
 const { PORT } = process.env
 const app = express()
 const server = http.Server(app)
 
 
-const swaggerUi = require('swagger-ui-express');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({type: 'application/json'}))
 
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use('/users', routes.user)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
-});
-
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
-});
-
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
-});
-
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+  return res.status(200).json({message: 'ok'});
 });
 
 
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,16 +1,9 @@
 import { Router } from "express"
-const users = {
-  1: {
-    id: 1,
-    name: 'Johan'
-  },
-  2: {
-    id: 2,
-    name: 'Mike'
-  },
-}
-const router = Router()
+import db from "../database"
+import { v4 as uuidv4 } from 'uuid'
 
+const DatabaseUrlUsers = 'server/users'
+const router = Router()
 /**
  * @openapi
  * /users:
@@ -20,18 +13,56 @@ const router = Router()
  *       200:
  *         description: Returns a mysterious string.
  */
-router.get('/', (req, res) => {
-  return res.send(Object.values(users));
+router.get('/', async (req, res) => {
+  const ref = db.ref(DatabaseUrlUsers);
+  let snapshot = await ref.once('value');
+  const users = snapshot.val() ? Object.values(snapshot.val()) : []
+  return res.status(200).json(users);
 });
+
 /**
  * @openapi
- * /users/{userId}:
+ * /users:
+ *   post:
+ *    parameters:
+ *         - in: path
+ *           name: login
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: Логин пользователя
+ *         - in: path
+ *           name: password
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: Пароль пользователя
+ *    description: Добавление пользователя
+ *    responses:
+ *      200:
+ *        description: что-то возвращает
+ */
+
+router.post('/', async (req, res) => {
+  const ref = db.ref(DatabaseUrlUsers);
+  const user = {
+    ...req.body,
+    id: uuidv4()
+  }
+  await ref.set({[user.login]: user})
+
+  return res.status(200).json(user);
+});
+
+/**
+ * @openapi
+ * /users/{login}:
  *   get:
  *     parameters:
  *         - in: path
- *           name: userId
+ *           name: login
  *           schema:
- *             type: integer
+ *             type: string
  *           required: true
  *           description: Numeric ID of the user to get
  *     description: Welcome to swagger-jsdoc!
@@ -39,8 +70,10 @@ router.get('/', (req, res) => {
  *       200:
  *         description: Returns a mysterious string.
  */
-router.get('/:userId', (req, res) => {
-  return res.send(users[req.params.userId])
+router.get('/:login', async (req, res) => {
+  const ref = db.ref(`${DatabaseUrlUsers}/${req.params.login}`);
+  let snapshot = await ref.once('value');
+  return res.status(200).json(snapshot.val());
 })
 
 export default router;
